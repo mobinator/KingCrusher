d9acb2ae70701290e742cfcc2b3e5acb9e7b03da
NicolasRausch <nicolas.rausch@deutschebahn.com>
2023-10-04 20:11:36 +0200

    send recive messages now

--- p2pagent.py
+++ p2pagent.py
@@ -1,14 +1,24 @@
 import socket
 import threading
 
-toni = ('192.168.1.7', 50001)
-nico = ('192.168.1.3', 50001)
+toni = ('192.168.1.7', 50002)
+nico = ('192.168.1.3', 50002)
 
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-my_socket.bind(('0.0.0.0', 50001))
-my_socket.sendto(b'0', toni)
+
+def receive_messages(sock):
+    while True:
+        data, address = sock.recvfrom(128)
+        print(f"Received message from {address[0]}: {data.decode()}")
+
+
+mySocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+mySocket.bind(('0.0.0.0', 50002))
+
+receiver_thread = threading.Thread(target=receive_messages, args=(mySocket,))
+receiver_thread.daemon = True
+receiver_thread.start()
 
 while True:
-    my_socket.sendto(input("> ").encode(), toni)
+    message = input("> ")
 
-
+    mySocket.sendto(message.encode(), toni)
