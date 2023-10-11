6852d1ebe513fedb60322aca8c346f50c45705fe
Toni <61843414+TonniBr@users.noreply.github.com>
2023-10-11 17:15:27 +0200

    broken boulder. needs fixing

--- Boulder.py
+++ Boulder.py
@@ -1,21 +1,53 @@
-import pygame.draw
-
+import pygame
 from Empty import *
-
 
 class Boulder(CollisionShape2D):
 
     def __init__(self, center, charge, inherited_speed: Vector2):
         super().__init__(center, Vector2(charge*10))
 
-        if inherited_speed.length() > 0:
-            self.direction = Vector2(0, -1) + inherited_speed/2
+        self.charge = charge
+        self.animation_images = self.load_images()
+        self.current_image_index = len(self.animation_images) - 1  # Starte mit dem letzten Bild
+        self.animation_timer = pygame.time.get_ticks()  # Timer für Animation
+        self.animation_delay = 100  # Millisekunden zwischen den Bildwechseln
 
         self.direction = Vector2(0, -1)
-        self.speed = 8 - charge
+
+        if inherited_speed.length() > 0:
+            self.direction += inherited_speed/2
+
+        self.speed = 8 - charge/2
+    
+        
+        self.rect = pygame.Rect(self.pos.x, self.pos.y, self.size.x, self.size.y)
+
+    def load_images(self):
+        # Basierend auf dem charge Wert den richtigen Pfad und Bilderanzahl festlegen
+        if 1 <= self.charge <= 2:
+            path = "assets/attacks/small"
+        elif 3 <= self.charge <= 4:
+            path = "assets/attacks/medium"
+        else:
+            path = "assets/attacks/large"
+
+        # Lade Bilder und skaliere sie
+        scale_factor = pygame.display.get_surface().get_size()[0] / 612
+        size = (int(80 * scale_factor), int(80 * scale_factor))
+        images = [pygame.transform.scale(pygame.image.load(f"{path}/{i}.png"), size) for i in range(4, 0, -1)]
+
+        return images
 
     def update(self, events):
         self.move(self.direction, self.speed)
 
+        # Update Animation
+        now = pygame.time.get_ticks()
+        if now - self.animation_timer > self.animation_delay:
+            self.current_image_index = (self.current_image_index + 1) % len(self.animation_images)
+            self.animation_timer = now
+
     def draw(self, win):
-        pygame.draw.rect(win, (0, 200, 0), (self.pos, self.size))
+        current_image = self.animation_images[self.current_image_index]
+        # Hier benutzen wir topleft statt pos, weil pygame.Rect die obere linke Ecke für das Zeichnen erwartet
+        win.blit(current_image, self.rect.topleft)
