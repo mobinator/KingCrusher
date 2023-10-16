fb8d616005d4af8511511a934e560c13b936e95c
mobinator <61843499+mobinator@users.noreply.github.com>
2023-10-16 13:17:47 +0200

    Added Send function

--- Networking.py
+++ Networking.py
@@ -10,10 +10,20 @@ class Networking:
         self.ownIP = VsIP.get_local_ip()
         self.enemyIP = None
         self.socketNum = 50002
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket = None
 
     def set_enemy_ip(self, ip):
         self.enemyIP = ip
 
     def begin(self):
-        self.socket.bind(('0.0.0.0', 50002))
+        if self.socket is None:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            self.socket.bind(('0.0.0.0', 50002))
+
+    def end(self):
+        self.socket.close()
+
+    def send(self, message):
+        if self.socket:
+            print("sending: ", message)
+            self.socket.sendto(message.encode(), (self.enemyIP, 50002))
