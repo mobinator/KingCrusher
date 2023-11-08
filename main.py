32f91f5e2ac357d90bd5c4d93772578edaedc2a6
NicolasRausch <nicolas.rausch@deutschebahn.com>
2023-11-08 09:21:21 +0100

    replaced match with if statement

--- main.py
+++ main.py
@@ -34,52 +34,49 @@ while True:
             pygame.quit()
             exit()
 
-    match state[0]:
-        case "MAIN_MENU":
-            menu.check_button_hover(pygame.mouse.get_pos())
-            menu.draw(win)
-            for event in events:
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    if menu.check_button_click(event.pos):
-                        ip_input.load(win)
-                        state[0] = "VS_IP"
+    if state[0] == "MAIN_MENU":
+        menu.check_button_hover(pygame.mouse.get_pos())
+        menu.draw(win)
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if menu.check_button_click(event.pos):
+                    ip_input.load(win)
+                    state[0] = "VS_IP"
 
-        case "VS_IP":
-            ip_input.draw(win)
-            ip_input.check_for_enter(state)
-            for event in events:
-                if event.type == pygame.MOUSEBUTTONDOWN or event.type == pygame.MOUSEMOTION or event.type == pygame.MOUSEBUTTONUP or event.type == event.type == pygame.KEYDOWN:
-                    if event.type == pygame.KEYDOWN:
-                        if event.key == pygame.K_RETURN:
-                            game.networking.begin(game)
-
-                    ip_input.handle_event(event, state)
-
-        case "SETTINGS":
-            settings.check_button_hover(pygame.mouse.get_pos())
-            settings.draw(win)
-            for event in events:
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    if settings.check_button_click(event.pos):
-                        pass
-                    elif settings.check_back_button_click(event.pos):
-                        state[0] = "MAIN_MENU"
-                        game.minimap.load()
-
-        case "GAME":
-            game.update(events)
-            game.draw(win)
-            for event in events:
+    elif state[0] == "VS_IP":
+        ip_input.draw(win)
+        ip_input.check_for_enter(state)
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONDOWN or event.type == pygame.MOUSEMOTION or event.type == pygame.MOUSEBUTTONUP or event.type == event.type == pygame.KEYDOWN:
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        escape_menu.show()
-                    if event.key == pygame.K_F5:  # F5 zum Speichern
-                        JSONHandler.save_game_to_json(game)
-                escape_menu.handle_event(event)
-            escape_menu.draw(win)
+                    if event.key == pygame.K_RETURN:
+                        game.networking.begin(game)
 
-            networking.send(str(game.player))
+                ip_input.handle_event(event, state)
 
-            # print(game.enemy_player.pos, game.player.pos)
+    elif state[0] == "SETTINGS":
+        settings.check_button_hover(pygame.mouse.get_pos())
+        settings.draw(win)
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if settings.check_button_click(event.pos):
+                    pass
+                elif settings.check_back_button_click(event.pos):
+                    state[0] = "MAIN_MENU"
+                    game.minimap.load()
+
+    elif state[0] == "GAME":
+        game.update(events)
+        game.draw(win)
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    escape_menu.show()
+            escape_menu.handle_event(event)
+        escape_menu.draw(win)
+
+        networking.send(str(game.player))
+
+        # print(game.enemy_player.pos, game.player.pos)
 
     pygame.display.update()
